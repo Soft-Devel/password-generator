f34c07b267e13df056fb5fb32b1ebe87ec2253cc
Software <55256368+Soft-Devel@users.noreply.github.com>
2021-01-10 00:49:53 -0800

    finish pw logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,61 @@
 // Assignment code here
-
-function generatePassword(){
+function generatePassword() {
+//user input prompting
   window.alert("Click OK to Start Generating Your New Password!");
-  let pwLength = prompt("How Many Characters Do You Want Your Password to Have? (8 Characters to 128)");
-  let lowerCase=prompt("Type 1 to Include Lower Case Letters");
-  let upperCase=prompt("Type 2 to Include Upper Case Letters");
-  let numbers=prompt("Type 3 to Include Numbers.");
-  let specialCharacters=prompt("Type 4 to Include Special Characters");
-
-  console.log (pwLength, lowerCase)
-
-}
-
-let lc = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-];
-
-let uc = [
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-];
-
-let num = [
-  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-];
-
-let chars = [
-  " ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", 
-  "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~" 
-];
-
-for (i=0; i < lc.length; i++) {
+  let pwLength = window.prompt("How Many Characters Do You Want Your Password to Have? (8 Characters to 128)");
+  var pwType = window.prompt("Type 1 for All Lower Case Letters, Type 2 for Lower and Upper Case, Type 3 for Lower, Upper Case and Numbers, Type 4 for Lower, Upper, Numbers and Special Characters");
   
-}
+  if (pwType ===null || pwType === "" || isNaN(pwType)) {
+    window.alert("You Need to Provide a Valid Answer!  Please Try Again.");
+    return generatePassword();
+  } 
 
-let passwd = 
+  pwType = parseInt(pwType);
+
+  //password character types
+
+  let lc = "abcdefghijklmnopqrstuvwxyz";
+
+  let uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let num = "0123456789";
+
+  let chars = ' !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 
-  // Get references to the #generate element
+//function to generate password
+
+
+  password = "";
+  passwordCharSet = "";
+  if (pwType===1) {
+    passwordCharSet +=lc;
+  }
+      
+  if (pwType===2) {
+    passwordCharSet += lc +=uc;
+  }
+
+  if (pwType===3) {
+    passwordCharSet += lc += uc +=num;
+  }  
+  
+  if (pwType===4) {
+    passwordCharSet += lc += uc +=num +=chars;
+  }
+
+  plength = parseInt(pwLength);
+
+  for (let i=0; i < plength; i++) {
+    password += passwordCharSet.charAt(
+      Math.floor(Math.random() * passwordCharSet.length)
+    );
+  }
+  console.log(password);
+  return password;
+};
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
